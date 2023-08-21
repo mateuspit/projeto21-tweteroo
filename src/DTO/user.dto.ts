099116549573192ed05e1936d5499d4cdf744a11
Mateus Barcelos <mateuspit@hotmail.com>
2023-08-21 16:05:21 -0300

    fix: correct errors when signup

--- src/DTO/user.dto.ts
+++ src/DTO/user.dto.ts
@@ -2,11 +2,8 @@ import { IsString, IsNotEmpty } from "class-validator";
 import { IsUrl } from "class-validator"
 
 export class NewUserDTO {
-    @IsString({ message: "The field is required!" })
-    @IsNotEmpty({ message: "The field is required!" })
+    @IsNotEmpty({ message: "All fields are required!" })
     username: string;
-
-    @IsUrl({ require_protocol: true }, { message: "The field is required!" })
-    @IsNotEmpty({ message: "The field is required!" })
+    @IsNotEmpty({ message: "All fields are required!" })
     avatar: string;
 }
